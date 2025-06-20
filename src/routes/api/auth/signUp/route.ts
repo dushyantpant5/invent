c32484ae31f2519f4fb3d6cf0786fdfaf37a0737
Hridayansh Sah <87271258+Hirdiyansh@users.noreply.github.com>
2025-06-20 18:30:31 +0530

    addes expiresat in route

--- src/routes/api/auth/signUp/route.ts
+++ src/routes/api/auth/signUp/route.ts
@@ -63,6 +63,7 @@ export async function POST(request: Request) {
         refreshTokenHash: refreshToken,
         userAgent: userAgent,
         ipAddress: ipAddress,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       },
     });
 
