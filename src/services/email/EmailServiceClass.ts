8eada34dcfe70d962e48e8d72d5175acd6810465
Dushyant <dushyantpant5@gmail.com>
2025-07-07 22:50:14 +0530

    supress email service

--- src/services/email/EmailServiceClass.ts
+++ src/services/email/EmailServiceClass.ts
@@ -1,36 +1,36 @@
-interface SendEmailParams {
-  toEmail: string;
-  otp: string;
-}
+// interface SendEmailParams {
+//   toEmail: string;
+//   otp: string;
+// }
 
-export class EmailServiceClass {
-  private serviceID: string;
-  private templateID: string;
-  private userID: string;
+// export class EmailServiceClass {
+//   private serviceID: string;
+//   private templateID: string;
+//   private userID: string;
 
-  constructor({
-    serviceID,
-    templateID,
-    userID,
-  }: {
-    serviceID: string;
-    templateID: string;
-    userID: string;
-  }) {
-    this.serviceID = serviceID;
-    this.templateID = templateID;
-    this.userID = userID;
-  }
+//   constructor({
+//     serviceID,
+//     templateID,
+//     userID,
+//   }: {
+//     serviceID: string;
+//     templateID: string;
+//     userID: string;
+//   }) {
+//     this.serviceID = serviceID;
+//     this.templateID = templateID;
+//     this.userID = userID;
+//   }
 
-  // async sendOtpEmail({ toEmail, otp }: SendEmailParams): Promise<void> {
-  //   await emailjs.send(
-  //     this.serviceID,
-  //     this.templateID,
-  //     {
-  //       email: toEmail,
-  //       passcode: otp,
-  //     },
-  //     this.userID
-  //   );
-  // }
-}
+//   // async sendOtpEmail({ toEmail, otp }: SendEmailParams): Promise<void> {
+//   //   await emailjs.send(
+//   //     this.serviceID,
+//   //     this.templateID,
+//   //     {
+//   //       email: toEmail,
+//   //       passcode: otp,
+//   //     },
+//   //     this.userID
+//   //   );
+//   // }
+// }
