e35feae4d8ab68984a29e79b6c60ae1c72bdf974
Hridayansh Sah <87271258+Hirdiyansh@users.noreply.github.com>
2025-07-12 13:54:57 +0530

    fixed lint

--- src/app/auth/authError/page.tsx
+++ src/app/auth/authError/page.tsx
@@ -1,14 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { AlertTriangle } from "lucide-react";
+import { AlertTriangle } from 'lucide-react';
 import Link from 'next/link';
 
+import { Button } from '@/components/ui/button';
+
 export default function AuthError() {
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <AlertTriangle className="w-14 h-14 text-black" />
-           <div className="font-bold text-black text-9xl ">401</div> 
-           <div className="text-1.5xl mb-2 mt-2">Oops, you don't have permission to access this page.<br/> Please check your credentials and try again.</div>
-           <Link href="/"><Button className="mt-2">Back to Home</Button></Link>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <AlertTriangle className="w-14 h-14 text-black" />
+      <div className="font-bold text-black text-9xl ">401</div>
+      <div className="text-1.5xl mb-2 mt-2">
+        Oops, you don't have permission to access this page.
+        <br /> Please check your credentials and try again.
+      </div>
+      <Link href="/">
+        <Button className="mt-2">Back to Home</Button>
+      </Link>
+    </div>
+  );
 }
