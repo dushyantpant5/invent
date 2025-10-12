6400d461712e5c9c43e0b97f5ede8a865309966a
Dushyant <dushyantpant5@gmail.com>
2025-10-12 21:57:53 +0530

    build fix

--- src/uiRoutes/api/product/product.queries.ts
+++ src/uiRoutes/api/product/product.queries.ts
@@ -1,12 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { getAllProducts, getProductById } from './product.api';
+import { getAllProducts } from './product.api';
 
 export const useProducts = () => useQuery({ queryKey: ['products'], queryFn: getAllProducts });
-
-export const useProduct = (id: string) =>
-  useQuery({
-    queryKey: ['product', id],
-    queryFn: () => getProductById(id),
-    enabled: !!id,
-  });
